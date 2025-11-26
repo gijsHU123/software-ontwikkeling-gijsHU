a7d112fbfba5cd39f448152f1419e70618f44a6f
gijsHU123 <gijs.devries@student.hu.nl>
2025-11-26 11:05:39 +0100

    added gitignore

--- Debug/Core/Src/front_layer.d
+++
@@ -1,3 +0,0 @@
-Core/Src/front_layer.o: ../Core/Src/front_layer.c \
- ../Core/Inc/front_layer.h
-../Core/Inc/front_layer.h:
--- Debug/Core/Src/io_layer.d
+++
@@ -1,2 +0,0 @@
-Core/Src/io_layer.o: ../Core/Src/io_layer.c ../Core/Inc/io_layer.h
-../Core/Inc/io_layer.h:
